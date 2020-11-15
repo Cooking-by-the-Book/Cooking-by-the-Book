d2efe67d06931019f71c6209c9b0e57c7477fc6a
Jh0m4s <jothomas@csumb.edu>
2020-11-14 22:52:08 -0800

    Changed .gitignore to actually the project, implemented login/logout with persistent signin. Also there are errors for incorrect credientials and usernames that already exist

--- Cooking-by-the-Book/Cooking-by-the-Book/AppDelegate.swift
+++ Cooking-by-the-Book/Cooking-by-the-Book/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "h6G3NBs6O6M3TYZ54LhxXhTuCO7WSqQAm0ePUgFV" // <- UPDATE
+                    $0.clientKey = "GhuReMQyI0iSUpDTVySSC3zBAplTlzCBh3mrO2nw" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
