d0801217326127099683c1ce6d5e803767769325
majinE <edgreyes@csumb.edu>
2020-11-23 16:59:18 -0800

    Deleted extra Navigation Controller, made temporary profile page.

---
+++ Cooking-by-the-Book/Cooking-by-the-Book/UserPostCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserPostCell.swift
+//  Cooking-by-the-Book
+//
+//  Created by Edgar Reyes on 11/22/20.
+//  Copyright © 2020 Cooking by the Book. All rights reserved.
+//
+
+import UIKit
+
+class UserPostCell: UICollectionViewCell {
+    
+    @IBOutlet weak var UserThumbail: UIImageView!
+
+}
