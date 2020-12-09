68a532ce72c9a1254b8f8e87b95ef05f57ae7abc
majinE <edgreyes@csumb.edu>
2020-12-08 20:33:00 -0800

    Saved Recipe Page added

---
+++ Cooking-by-the-Book/Cooking-by-the-Book/SavedPostCell.swift
@@ -0,0 +1,14 @@
+//
+//  SavedPostCell.swift
+//  Cooking-by-the-Book
+//
+//  Created by Edgar Reyes on 12/8/20.
+//  Copyright © 2020 Cooking by the Book. All rights reserved.
+//
+
+import UIKit
+
+class SavedPostCell: UICollectionViewCell {
+    
+    @IBOutlet weak var savedThumbnail: UIImageView!
+}
--- Cooking-by-the-Book/Cooking-by-the-Book/SavedPostViewController.swift
+++ Cooking-by-the-Book/Cooking-by-the-Book/SavedPostViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ProfileViewController.swift
+//  SavedPostViewController.swift
 //  Cooking-by-the-Book
 //
-//  Created by Joey Thomas on 11/14/20.
+//  Created by Edgar Reyes on 12/8/20.
 //  Copyright © 2020 Cooking by the Book. All rights reserved.
 //
 
@@ -10,11 +10,10 @@ import UIKit
 import Parse
 import AlamofireImage
 
-class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-
+class SavedPostViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
     var posts = [PFObject]()
     
-    @IBOutlet weak var username: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -24,7 +23,7 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
         collectionView.dataSource = self
         
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        
+
         layout.minimumLineSpacing = 2
         layout.minimumInteritemSpacing = 0
         let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 2
@@ -32,13 +31,16 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        let currentUser = PFUser.current()
+        let objects = (currentUser?["savedPosts"] as? [PFObject]) ?? []
+        var objectIds:[String] = []
+        for object in objects {
+            objectIds.append(object.objectId!)
+        }
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
-        query.whereKey("author", equalTo: PFUser.current())
-        query.order(byDescending: "createdAt")
-        query.limit = 20
-        
-        query.findObjectsInBackground { (posts, error) in
+        query.whereKey("objectId", containedIn: objectIds)
+        query.findObjectsInBackground {(posts, error) in
             if posts != nil {
                 self.posts = posts!
                 self.collectionView.reloadData()
@@ -47,41 +49,23 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
+
         return posts.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserPostCell", for: indexPath) as! UserPostCell
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SavedPostCell", for: indexPath) as! SavedPostCell
         let post = posts[indexPath.row]
-        
-        let user = post["author"] as! PFUser
-        username.text = user.username
-        
+
         let postImage = post["image"] as! PFFileObject
         let urlString = postImage.url!
         let url = URL(string: urlString)!
-        
-        cell.UserThumbail.af.setImage(withURL: url)
+
+        cell.savedThumbnail.af.setImage(withURL: url)
         return cell
     }
-    
-    @IBAction func onSignOut(_ sender: Any) {
-        
-        PFUser.logOut()
-        
-        let main = UIStoryboard(name: "Main", bundle: nil)
-        let mainPageViewController = main.instantiateViewController(identifier: "MainPageViewController")
-        
-        let delegate = self.view.window?.windowScene?.delegate as! SceneDelegate
-        delegate.window?.rootViewController = mainPageViewController
-    }
-    
-    
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
@@ -94,6 +78,4 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
         
         detailedViewController.recipe = recipe
     }
-    
-
 }
